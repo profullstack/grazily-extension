f3413a44730704045a5c4a6694ea764fc3841e52
Anthony Ettinger <anthony@chovy.com>
2023-03-26 00:41:55 -0700

    add background.js and contextmenu

--- src/base.js
+++ src/base.js
@@ -1,9 +1,20 @@
 export default class BaseElement extends HTMLElement {
   constructor() {
     super();
-    this.ns = "ea";
+    this.ns = "easyapplier";
+    this.handleExport = this.handleExport.bind(this);
+    this.importJsonData = this.importJsonData.bind(this);
+    this.loadCss = this.loadCSS.bind(this);
+    this.updateData = this.updateData.bind(this);
+    this.exportJsonData = this.exportJsonData.bind(this);
+
     this.attachShadow({ mode: "open" });
     this.loadCSS("./styles/main.css");
+
+    this.jsonData = JSON.parse(localStorage.getItem(this.ns)) || {
+      key1: "value1",
+      key2: "value2",
+    };
   }
 
   async loadCSS(url) {
@@ -14,5 +25,52 @@ export default class BaseElement extends HTMLElement {
     } catch (error) {
       console.error("Error loading CSS:", error);
     }
+  }
+
+  importJsonData(file) {
+    try {
+      const fileReader = new FileReader();
+      fileReader.readAsText(file);
+
+      fileReader.onload = async () => {
+        this.jsonData = JSON.parse(fileReader.result);
+        localStorage.setItem(this.ns, JSON.stringify(this.jsonData));
+        this.triggerUpdate();
+        console.log("Imported JSON data:", this.jsonData);
+      };
+    } catch (error) {
+      console.error("Error importing JSON file:", error);
+    }
+  }
+
+  updateData(key, value) {
+    console.log(key, value);
+    this.jsonData[key] = value;
+    localStorage.setItem(this.ns, JSON.stringify(this.jsonData));
+    this.triggerUpdate();
+  }
+
+  exportJsonData(filename = "data.json") {
+    const dataStr = JSON.stringify(this.jsonData, null, 2);
+    const blob = new Blob([dataStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = filename;
+    link.click();
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+      link.remove();
+    }, 0);
+  }
+
+  async triggerUpdate() {
+    const event = new CustomEvent("update");
+    this.dispatchEvent(event);
+  }
+
+  handleExport(e) {
+    e.preventDefault();
+    this.exportJsonData(this.ns + "-profile.json");
   }
 }
--- src/easy-applier.js
+++ src/easy-applier.js
@@ -4,12 +4,64 @@ class EasyApplier extends BaseElement {
   constructor() {
     super();
 
+    this.handleFileChange = this.handleFileChange.bind(this);
+    this.render = this.render.bind(this);
+    this.addEvents = this.addEvents.bind(this);
+    this.removeEvents = this.removeEvents.bind(this);
+  }
+
+  async render(initial = false) {
     this.shadowRoot.innerHTML = `
-        <style id="main-css"></style>
-        <div>
-          <h1>Hello from My Component</h1>
+        ${this.renderStyle()}
+
+        <div id="${this.ns}">
+          <h1>Easy Applier</h1>
+          <pre>${JSON.stringify(this.jsonData, null, 2)}</pre>
+          <button id="export">Export Profile</button>
+          Import profiles: <input type="file" id="json-file-input" accept="application/json" />
         </div>
       `;
+
+    await this.loadCSS("./styles/main.css");
+    this.removeEvents(initial);
+    this.addEvents();
+  }
+
+  async connectedCallback() {
+    await this.render(true);
+  }
+
+  renderStyle() {
+    return `<style id="main-css"></style>`;
+  }
+
+  addEvents() {
+    this.exportButton = this.shadowRoot.querySelector("#export");
+    this.exportButton.addEventListener("click", this.handleExport);
+    this.jsonFileInput = this.shadowRoot.querySelector("#json-file-input");
+    this.jsonFileInput.addEventListener("change", this.handleFileChange);
+    this.addEventListener("update", this.render);
+  }
+
+  removeEvents(initial = false) {
+    if (initial) return;
+    this.exportButton.removeEventListener("click", this.handleExport);
+    this.jsonFileInput.removeEventListener("change", this.handleFileChange);
+    this.removeEventListener("update", this.render);
+  }
+
+  disconnectedCallback() {
+    this.removeEvents();
+  }
+
+  async handleFileChange(event) {
+    console.log("here");
+    if (event.target.files.length > 0) {
+      console.log("here2");
+      const file = event.target.files[0];
+      this.importJsonData(file);
+      await this.render();
+    }
   }
 }
 
