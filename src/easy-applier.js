bede1d751db06e968655d2daf5ad3c9881f80773
Anthony Ettinger <anthony@chovy.com>
2023-03-25 20:36:23 -0700

    init

---
+++ src/easy-applier.js
@@ -0,0 +1,27 @@
+class EasyApplier extends HTMLElement {
+    constructor() {
+      super();
+  
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+  
+      shadowRoot.innerHTML = `
+        <style>
+          :host {
+            display: block;
+            width: 300px;
+            height: 200px;
+            background-color: #f0f0f0;
+            padding: 1rem;
+            border: 1px solid #ccc;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+          }
+        </style>
+        <div>
+          <h2>Hello from My Component</h2>
+        </div>
+      `;
+    }
+  }
+  
+  customElements.define('easy-applier', EasyApplier);
+  
